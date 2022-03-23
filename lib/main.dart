7bff2ea8e545527cdb9e37d6ed3677eae8fe8283
Scott Laing <fifersheep@gmail.com>
2022-03-23 21:55:05 +0000

    hook presentation up to supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/widgets.dart';
 import 'package:planning_poker/app.dart';
-// import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // final supabase = await Supabase.initialize(
-  //   url: 'https://vrryydwonwupwdbatvtk.supabase.co',
-  //   anonKey:
-  //       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZycnl5ZHdvbnd1cHdkYmF0dnRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5MDA2NTcsImV4cCI6MTk2MzQ3NjY1N30.6WMrzQxsWLZpRaAAxK3npcRirF6LOj-EPPO2pBaAMGo',
-  // );
+  await Supabase.initialize(
+    url: 'https://xjbpbchuunkoucygvrhs.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqYnBiY2h1dW5rb3VjeWd2cmhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5OTI1MDMsImV4cCI6MTk2MzU2ODUwM30.lBWuEeEI_fOJ4p58ecved3unq5NvUzgpsnZ2OcHUSfo',
+  );
 
-  runApp(App(
-    onSignupTapped: () async {
-      // await supabase.client.auth.signInWithProvider(
-      //   Provider.google,
-      //   options: AuthOptions(
-      //     redirectTo: null,
-      //   ),
-      // );
-    },
-  ));
+  runApp(App());
 }
--- lib/presentation_page.dart
+++ lib/presentation_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/widgets.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class PresentationPage extends StatelessWidget {
+class PresentationPage extends StatefulWidget {
   const PresentationPage({Key? key}) : super(key: key);
 
   @override
+  State<PresentationPage> createState() => _PresentationPageState();
+}
+
+class _PresentationPageState extends State<PresentationPage> {
+  final subscription = Supabase.instance.client
+      .from('participants')
+      .stream(['id'])
+      .order('name')
+      .execute();
+
+  @override
+  void dispose() {
+    Supabase.instance.client.removeAllSubscriptions(); // ???
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) => Center(
-        child: Text("Presentation"),
+        child: StreamBuilder(
+            stream: subscription,
+            builder: (
+              BuildContext context,
+              AsyncSnapshot<List<Map<String, dynamic>>> snapshot,
+            ) {
+              if (!snapshot.hasData ||
+                  snapshot.hasError ||
+                  snapshot.data!.isEmpty) {
+                return Container();
+              }
+
+              final participants = snapshot.data!.map((data) {
+                return Text("${data['name']} | ${data['vote']}");
+              }).toList();
+
+              return Column(
+                children: participants,
+              );
+            }),
       );
 }
