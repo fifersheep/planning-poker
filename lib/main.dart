51e546c8a6afc47f3f9dca670057e0d8834dfee7
Scott Laing <fifersheep@gmail.com>
2022-03-30 02:05:47 +0100

    try using env variables for Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  const url = String.fromEnvironment("SUPABASE_URL");
+  const pubKey = String.fromEnvironment("SUPABASE_PUB_KEY");
   await Supabase.initialize(
-    url: 'https://xjbpbchuunkoucygvrhs.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqYnBiY2h1dW5rb3VjeWd2cmhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5OTI1MDMsImV4cCI6MTk2MzU2ODUwM30.lBWuEeEI_fOJ4p58ecved3unq5NvUzgpsnZ2OcHUSfo',
+    url: url,
+    anonKey: pubKey,
   );
 
   runApp(App());
