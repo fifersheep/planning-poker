868549245974048034558bed0440cd2498d6204a
Scott Laing <fifersheep@gmail.com>
2023-10-17 23:11:55 +0100

    remove dotenv properly

--- lib/data/supabase_client_extensions.dart
+++ lib/data/supabase_client_extensions.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase/supabase.dart';
 
 extension SupabaseClientExtensions on SupabaseClient {
   static final instance = SupabaseClient(
-    (kDebugMode ? dotenv.env['SUPABASE_DEV_URL'] : dotenv.env['SUPABASE_URL']) ?? '',
-    (kDebugMode ? dotenv.env['SUPABASE_DEV_PUB_KEY'] : dotenv.env['SUPABASE_PUB_KEY']) ?? '',
+    const String.fromEnvironment(kDebugMode ? 'SUPABASE_DEV_URL' : 'SUPABASE_URL'),
+    const String.fromEnvironment(kDebugMode ? 'SUPABASE_DEV_PUB_KEY' : 'SUPABASE_PUB_KEY'),
   );
 }
